92f087215aca6b1512a07ae329e72d97dd78705d
Кирилл Гулиев <guliev@ecopsy.ru>
2022-02-10 10:34:01 +0300

    bench added

--- R/group_and_add_row_numbers.R
+++ R/group_and_add_row_numbers.R
@@ -19,3 +19,35 @@ setDT(data)
 
 setorder(data, qrt, month)
 data[, row_n := .I, by = .(qrt, month)]
+
+
+# Сравнение с dplyr и base R
+
+mbm <- microbenchmark::microbenchmark(
+  
+  base_R = {
+    
+    data$rank <- ave(seq_len(nrow(data)), data[, c("year","qrt")], FUN = seq_along)
+    
+  },
+  
+  r2evans = {
+    
+    library(dplyr)
+    data %>%
+      group_by(year, qrt) %>%
+      mutate(rank = row_number()) %>%
+      ungroup()
+  
+},
+
+  rg4s = {
+    
+    library(data.table)
+    setDT(data)
+    data[, row_n := .I, by = .(qrt, month)]
+},
+
+times = 100)
+
+ggplot2::autoplot(mbm)
